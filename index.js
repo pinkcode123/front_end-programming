23c15c22267bc87de6cb6a7816d4c9b3b7c8480c
pinkcode123 <ilesanmiryth209@gmail.com>
2023-08-23 22:21:30 +0100

    another attempt

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    $('button').click(function(){
-        $('p').css('text-transfrom', 'uppercase')
+    $('#upper').click(function(){
+        $('p').css('text-transform', 'uppercase')
+    })
+    $('#red').click(function(){
+        $('p').css('color', 'red')
+    })
+    $('#blue').click(function(){
+        $('p').css('color', 'blue')
+    })
+    $('#yellow').click(function(){
+        $('p').css('color', 'yellow')
     })
 })
